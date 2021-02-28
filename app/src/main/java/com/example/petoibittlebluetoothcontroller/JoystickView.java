1b2b382146a3121d35831121c3b8899c95052ba5
Enrique Moran <enriquemoran95@gmail.com>
2021-02-28 16:29:32 +0100

    First upload, test joystick controller

---
+++ app/src/main/java/com/example/petoibittlebluetoothcontroller/JoystickView.java
@@ -0,0 +1,118 @@
+package com.example.petoibittlebluetoothcontroller;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+
+/**************************************************************************
+ *  CODE TAKEN FROM https://github.com/efficientisoceles/JoystickView
+ *************************************************************************/
+
+public class JoystickView extends SurfaceView implements SurfaceHolder.Callback, View.OnTouchListener {
+
+    private float centerX=0;
+    private float centerY=0;
+    private float outerRadius=0;
+    private float innerRadius=0;
+    private JoystickListener joystickCallback;
+
+    public JoystickView(Context context) {
+        super(context);
+        getHolder().addCallback(this);
+        setOnTouchListener(this);
+        if(context instanceof JoystickListener)
+            joystickCallback = (JoystickListener) context;
+    }
+
+    public JoystickView(Context context, AttributeSet attributes, int style) {
+        super(context, attributes, style);
+        getHolder().addCallback(this);
+        setOnTouchListener(this);
+        if(context instanceof JoystickListener)
+            joystickCallback = (JoystickListener) context;
+    }
+
+    public JoystickView(Context context, AttributeSet attributes) {
+        super(context, attributes);
+        getHolder().addCallback(this);
+        setOnTouchListener(this);
+        if(context instanceof JoystickListener)
+            joystickCallback = (JoystickListener) context;
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        setupDimensions();
+        drawJoystick(this.centerX, this.centerY);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+    }
+
+    private void setupDimensions() {
+        this.centerX = getWidth() / 2;
+        this.centerY = getHeight() / 2;
+        outerRadius = Math.min(getWidth(), getHeight()) / 3;
+        innerRadius = Math.min(getWidth(), getHeight()) / 5;
+    }
+
+    private void drawJoystick(float newX, float newY) {
+        if(getHolder().getSurface().isValid()) {
+            Canvas canvas = this.getHolder().lockCanvas();
+            Paint paint = new Paint();
+            canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+
+            paint.setARGB(255, 50, 50, 50);
+            canvas.drawCircle(centerX, centerY, outerRadius, paint);
+
+            paint.setARGB(255, 0, 0, 255);
+            canvas.drawCircle(newX, newY, innerRadius, paint);
+
+            getHolder().unlockCanvasAndPost(canvas);
+        }
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        if(view.equals(this)) {
+            if(motionEvent.getAction() != MotionEvent.ACTION_UP) {
+                float displacement = (float) Math.sqrt((Math.pow(motionEvent.getX() - centerX, 2)) + Math.pow(motionEvent.getY() - centerY, 2));
+                if(displacement < innerRadius)
+                {
+                    drawJoystick(motionEvent.getX(), motionEvent.getY());
+                    joystickCallback.onJoystickMoved((motionEvent.getX() - centerX) / innerRadius, (motionEvent.getY() - centerY) / -innerRadius, getId());
+                }
+                else
+                {
+                    float ratio = innerRadius / displacement;
+                    float constrainedX = centerX + (motionEvent.getX() - centerX) * ratio;
+                    float constrainedY = centerY + (motionEvent.getY() - centerY) * ratio;
+                    drawJoystick(constrainedX, constrainedY);
+                    joystickCallback.onJoystickMoved((constrainedX-centerX) / innerRadius, (constrainedY-centerY) / -innerRadius, getId());
+                }
+            } else {
+                drawJoystick(this.centerX, this.centerY);
+                joystickCallback.onJoystickMoved(0, 0, getId());
+            }
+        }
+        return true;
+    }
+
+    public interface JoystickListener {
+        void onJoystickMoved(float xPercent, float yPercent, int id);
+    }
+}
---
+++ app/src/main/java/com/example/petoibittlebluetoothcontroller/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.petoibittlebluetoothcontroller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity implements JoystickView.JoystickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onJoystickMoved(float xPercent, float yPercent, int id) {
+        switch (id)
+        {
+            case R.id.joystickRight:
+                Log.d("Right Joystick", "X percent: " + xPercent + " Y percent: " + yPercent);
+                break;
+            case R.id.joystickLeft:
+                Log.d("Left Joystick", "X percent: " + xPercent + " Y percent: " + yPercent);
+                break;
+        }
+    }
+}
