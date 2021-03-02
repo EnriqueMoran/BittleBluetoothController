be55fb02818117bb63ccde58288f5b2dc1139d22
Enrique Moran <enriquemoran95@gmail.com>
2021-03-02 20:52:03 +0100

    Bluetooth module added

--- app/src/main/java/com/example/petoibittlebluetoothcontroller/MainActivity.java
+++ app/src/main/java/com/example/petoibittlebluetoothcontroller/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.petoibittlebluetoothcontroller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity implements JoystickView.JoystickListener{
+
+    Handler bluetoothIn;
+    Button btDisconnect;
+    final int handlerState=0;
+    private BluetoothAdapter btAdapter = null;
+    private BluetoothSocket btSocket = null;
+    private StringBuilder dataStringIn = new StringBuilder();
+    private ConnectedThread myConnectionBt;
+    private static final UUID BTMODULEUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    private static String address = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,18 +38,143 @@ public class MainActivity extends AppCompatActivity implements JoystickView.Joys
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
+
+        btDisconnect = findViewById(R.id.btDisconnect);
+
+        bluetoothIn = new Handler() {
+          public void handleMessage(android.os.Message msg) {
+              if(msg.what == handlerState) {
+                  Log.d("RECEIVED", (String) msg.obj);
+              }
+          }
+        };
+
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        verifyBTStatus();
+
+        btDisconnect.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                try {
+                    btSocket.close();
+                } catch (IOException e) {
+                    Toast.makeText(getBaseContext(), "Error disconnecting Bluetooth", Toast.LENGTH_SHORT).show();
+                }
+                finish();
+            }
+        });
     }
 
     @Override
     public void onJoystickMoved(float xPercent, float yPercent, int id) {
         switch (id)
         {
-            case R.id.joystickRight:
+           /* case R.id.joystickRight:
                 Log.d("Right Joystick", "X percent: " + xPercent + " Y percent: " + yPercent);
-                break;
+                break;*/
             case R.id.joystickLeft:
                 Log.d("Left Joystick", "X percent: " + xPercent + " Y percent: " + yPercent);
+
+              //  myConnectionBt.write("a");
                 break;
         }
     }
+
+    private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
+        return device.createRfcommSocketToServiceRecord(BTMODULEUUID);
+    }
+
+    public void onResume() {
+        super.onResume();
+
+        Intent intent = getIntent();
+        address = intent.getStringExtra(PairedDevices.EXTRA_DEVICE_ADDRESS);
+        BluetoothDevice device = null;
+
+        if(btAdapter != null) {
+            device = btAdapter.getRemoteDevice(address);
+        }
+
+        try {
+            btSocket = createBluetoothSocket(device);
+        } catch (IOException e) {
+            Toast.makeText(getBaseContext(), "Couldn't create socket.", Toast.LENGTH_LONG).show();
+        }
+
+        try {
+            btSocket.connect();
+        } catch (IOException e) {
+            try {
+                btSocket.close();
+            } catch (IOException e2) {
+
+            }
+        }
+        myConnectionBt = new ConnectedThread(btSocket);
+        myConnectionBt.start();
+    }
+
+    public void onPause() {
+        super.onPause();
+
+        try {
+            btSocket.close();
+        } catch (IOException e) {
+
+        }
+    }
+
+    private void verifyBTStatus() {
+        if (btAdapter == null) {
+            Toast.makeText(getBaseContext(), "Bluetooth not supported by device", Toast.LENGTH_LONG).show();
+        } else {
+            if(btAdapter.isEnabled()) {
+
+            } else {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, 1);
+            }
+        }
+    }
+
+    private class ConnectedThread extends Thread {
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedThread(BluetoothSocket socket) {
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+            try {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) {
+
+            }
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            byte[] byte_in = new byte[1];
+            while (true) {
+                try {
+                    mmInStream.read(byte_in);
+                    char ch = (char) byte_in[0];
+                    bluetoothIn.obtainMessage(handlerState, ch).sendToTarget();
+                } catch (IOException e) {
+                    break;
+                }
+            }
+        }
+
+        public void write(String input) {
+            try {
+                mmOutStream.write(input.getBytes());
+            } catch (IOException e) {
+                Toast.makeText(getBaseContext(), "Connection failed.", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
+    }
+
 }
+
---
+++ app/src/main/java/com/example/petoibittlebluetoothcontroller/PairedDevices.java
@@ -0,0 +1,88 @@
+package com.example.petoibittlebluetoothcontroller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Collections;
+import java.util.Set;
+
+/*************************************************************************************************
+ *  CODE TAKEN FROM https://www.youtube.com/watch?v=YqNIN4t7IuM&t=1689s&ab_channel=INNOVADOMOTICS
+ ************************************************************************************************/
+
+public class PairedDevices extends AppCompatActivity {
+
+    private static final String TAG = "PairedDevices";
+    ListView idList;
+    public static String EXTRA_DEVICE_ADDRESS = "device_address";
+
+    private BluetoothAdapter m_bt_adapter;
+    private ArrayAdapter m_paired_devices;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_pairing);
+    }
+
+    public void onResume() {
+        super.onResume();
+
+        verifyBTStatus();
+        m_paired_devices = new ArrayAdapter(this, R.layout.found_devices);
+        idList = (ListView) findViewById(R.id.idList);
+        idList.setAdapter(m_paired_devices);
+        idList.setOnItemClickListener(m_device_click_listener);
+        m_bt_adapter = BluetoothAdapter.getDefaultAdapter();
+
+        Set<BluetoothDevice> paired_devices = Collections.emptySet();
+        if(m_bt_adapter != null) {
+            paired_devices = m_bt_adapter.getBondedDevices();
+        }
+        if(paired_devices.size() > 0) {
+            for(BluetoothDevice device : paired_devices) {
+                m_paired_devices.add(device.getName() + "\n" + device.getAddress());
+            }
+        }
+    }
+
+    private AdapterView.OnItemClickListener m_device_click_listener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            String info = ((TextView) view).getText().toString();
+            String address = info.substring(info.length() - 17);
+
+            finishAffinity();
+
+            Intent intent = new Intent(PairedDevices.this, MainActivity.class);
+            intent.putExtra(EXTRA_DEVICE_ADDRESS, address);
+            startActivity(intent);
+        }
+    };
+
+    private void verifyBTStatus() {
+        m_bt_adapter = BluetoothAdapter.getDefaultAdapter();
+
+        if(m_bt_adapter == null) {
+            Toast.makeText(getBaseContext(), "Bluetooth not supported by device.", Toast.LENGTH_SHORT).show();
+        } else {
+            if(m_bt_adapter.isEnabled()) {
+                Log.d(TAG, "BLuetooth already enabled");
+            } else {
+                Intent enable_BT_intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enable_BT_intent, 1);
+            }
+        }
+    }
+}
